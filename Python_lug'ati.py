ba9d2d056c67879cc41f290978e95758cddaf09c
GGit001hub <122269399+GGit001hub@users.noreply.github.com>
2023-01-29 16:49:20 +0500

    Add files via upload

---
+++ Python_lug'ati.py
@@ -0,0 +1,215 @@
+"""
+1) print() - malumotni console chiqarib beruvchi funksiya
+
+2) o'zgaruvchi - kompyuter xotirasidamalum bir malumot uchun berilgan joy
+
+3) o'zgaruvchilar turlari - 3 ta
+
+4) intiger - butun son  >int
+
+5) float - o'nlik son >float
+
+6) string - matn      >str
+
+7) f"" - matn va o'zgaruvchilarni birgalikda ishlatishda imkon beruvchi funksiya
+
+8) upper() - so'zlardagi barcha harflarni katta qilib chiqarib beradi
+
+9) lower() -  so'zlardagi har bir harfni kichkia qilib chqarib beradi
+
+10) capitaliz() - faqat birinchi so'zdagi birinchi harfni katta qilib beradi
+
+11) title - har bir so'zdagi birinchi  harfni katta qilib beradi
+
+12) lstrip - so'zning chap tomonidagi bo'shliqni olib tashlaydi
+
+13) rstrip - so'zning o'ng tomonidagi bo'shliqni olib tashlaydi
+
+14) strip - ikkala tomondagi bo'shliqni olib tashlaydi
+
+15) input() - foydalanuvchidan malumotni olish va o'zgaruvchiga yuklash
+                                                  vazifasini bajaruvchi funksiya
+16) append() - Listning oxiriga lug'at qo'shish funksiyasi
+
+17) insert() - Listni xohlagan joyiga lug'at qo'shish funksiyasi
+
+18) del() - Listdan lug'atni o'chirish funksiysiya
+
+19) remove() - Ro'yxatdan elementni qiymati orqali o'chirish
+
+20) pop() - Lug'atni birinchi listdan ikkinchi listga ko'chiuvchi funksiya
+
+21) sort() - Ro'yxatni alifbo tartibida chiqarish
+
+22) sorted( - Ro'yxatni bir marta alifbo tartibida chiqarish
+   
+23) reverse=true - Ro'yxatni alifbo bo'yicha teskari holatda chiqarish 
+   
+24) reverse() - Ro'yxatni teskari aylantirib beruvchi fuunksiya
+   
+25) len() - Ro'yxatni uzunligini chiqarib beruvchi funksiya
+   
+26) lis(range()) - list yaratib ichiga ro'yxat yaratuvchi funksiya
+   
+27) min() - berilgan sonlar ichidagi eng kichik sonni qaytaradi
+   
+28) max() - berilgan sonlar ichidagi eng katta sonni qaytaradi
+   
+29) sum() - berilgan barcha sonlarni yig'indisini hisoblovchi funksiya
+   
+30) for() - Takrorlovchi operators
+   
+31) if - shart 
+   
+32) lug'at - ikki qismdan iborat. 1-'kalit' 2-'qiymat
+   
+33) lug'atda (index) o'rniga (kalit) dan foydalanamiz
+   
+34) or - yoki 
+   
+35) get - ( kalit mavjut emas)
+   
+36) item - metodi lug'atdagi qimatlarni hamasini qaytaradi
+
+37) key() - lug'atdagi kalit so'zlarni qaytaradi           
+   
+38)value() - lug'tdagi qiymatlarni qaytaradi
+   
+39)if - for ni o'rniga ishlatiladi         
+   
+40)random - modul - tasodifiy son tanlaydi
+
+41) random - 0 dan 1 gacha float sonlarni qaytaradi
+   
+42) randint(a,b) - a va b oralig'idagi sonlarni oladi         
+   
+43) randrange(a,b,c) - a dan b gacha c qadamdan sonlarni oladi 
+   
+44) choice() - ro'yxatdan tasodifiy elementni qaytaradi
+   
+45) set() - matndagi bitta harf bir nechi marotaba kegan bo'lsa 1 tasini oladi         
+   
+46)            
+   
+47)       
+   
+48)          
+   
+49)
+           
+50)
+  
+51)  
+  
+  
+  
+  
+   
+   
+
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+           
+"""
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
